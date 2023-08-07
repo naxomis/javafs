875f06a2bbf39698bff3e0ef327596a90a4e5635
naxomis <nayomi@hs.ac.kr>
2023-08-07 17:51:20 +0900

    0807

---
+++ shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -0,0 +1,34 @@
+package com.shop.controller;
+
+import com.shop.dto.ItemDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@Controller
+@RequestMapping(value = "thymeleaf")
+public class ThymeleafExController {
+
+
+    @GetMapping("/ex01")
+    public String thymeleafExample01(Model model){
+        model.addAttribute("data", "타임리프 예제 입니다.");
+        return "thymeleafEx/thymeleafEx01";
+    }
+
+    @GetMapping("/ex02")
+    public String thymeleafExample02(Model model){
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
+    }
+}
